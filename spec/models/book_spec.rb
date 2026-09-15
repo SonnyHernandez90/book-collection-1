8b951bacb8f56d8a9990fe2535627eae5791acb2
Sonny Hernandez <SonnyHernandez90@users.noreply.github.com>
2026-09-15 02:42:36 +0000

    Add failing specs for additional book attributes

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -9,4 +9,23 @@ RSpec.describe Book, type: :model do
     expect(blank_book).not_to be_valid
     expect(blank_book.errors[:title]).to include("can't be blank")
   end
+
+  it "stores an author" do
+    book = Book.new(title: "Clean Code", author: "Robert C. Martin")
+
+    expect(book.author).to eq("Robert C. Martin")
+  end
+
+  it "stores a numerical price" do
+    book = Book.new(title: "Clean Code", price: 39.99)
+
+    expect(book.price).to eq(39.99)
+  end
+
+  it "stores a published date" do
+    date = Date.new(2008, 8, 1)
+    book = Book.new(title: "Clean Code", published_date: date)
+
+    expect(book.published_date).to eq(date)
+  end
 end
--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -20,4 +20,30 @@ RSpec.describe "Books", type: :request do
     expect(response).to have_http_status(:unprocessable_content)
     expect(flash[:alert]).to eq("Title cannot be blank.")
   end
+
+  it "creates a book with an author" do
+    post books_path, params: {
+      book: { title: "Clean Code", author: "Robert C. Martin" }
+    }
+
+    expect(Book.last.author).to eq("Robert C. Martin")
+  end
+
+  it "creates a book with a numerical price" do
+    post books_path, params: {
+      book: { title: "Clean Code", price: 39.99 }
+    }
+
+    expect(Book.last.price).to eq(39.99)
+  end
+
+  it "creates a book with a published date" do
+    date = Date.new(2008, 8, 1)
+
+    post books_path, params: {
+      book: { title: "Clean Code", published_date: date }
+    }
+
+    expect(Book.last.published_date).to eq(date)
+  end
 end
